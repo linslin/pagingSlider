f35ebf5e87c557a8dbb167fc117007df2650a67b
Nils Gajsek <nils.gajsek@glanzkinder.com>
2014-01-22 16:28:43 +0100

    Commiting first part of sourcefiles

---
+++ app/lib/quoJs/README.md
@@ -0,0 +1,25 @@
+# QuoJS 2.3.1
+### Micro #JavaScript Library for Mobile Devices
+
+## QuoJS?
+QuoJS is a micro, modular, Object-Oriented and concise JavaScript Library that simplifies HTML document traversing, event handling, and Ajax interactions for rapid mobile web development. It allows you to write powerful, flexible and cross-browser code with its elegant, well documented, and micro coherent API.
+
+Designed to change the way that you write JavaScript with the goal: a 5-6k library that handles most basic drudge work with a nice API so you can concentrate on getting stuff done.
+Released under the MIT license, which gives you the possibility to use it and modify it in every circumstance.
+
+[Source code](https://github.com/soyjavi/QuoJS) and [issue tracking](https://github.com/soyjavi/QuoJS/issues) are available on Github.
+
+## Learn more about the project
+The idea of QuoJs arose earlier this year, the author [Javi Jiménez Villar](http://twitter.com/soyjavi) saw how existing JavaScript Libraries were not optimized for mobile devices. They were very big libraries that were built based on requirements of desktop devices, so mobile performance was not optimal.
+These libraries doesn't have a good support to touch events such as: touch, tap, swipe ... or a semantic API that would help the developer to create a good & cool JavaScript.
+
+- Visit QuoJS [Site](http://quojs.tapquo.com/)
+
+## Credits
+Created by [Javier Jiménez](http://twitter.com/soyjavi).
+Copyright (c) 2011, 2013 by Tapquo Inc.
+
+## Licensing Options
+Released under MIT license, https://raw.github.com/soyjavi/QuoJS/master/LICENSE.txt
+
+See LICENSE.txt for license.
---
+++ app/pagingSlider.js
@@ -0,0 +1,191 @@
+/**
+ * pagingSlider.js - 
+ *
+ * @category  Mobile-Hybrid
+ * @package   pagingSlider
+ * @author    Nils Gajsek <nils.gajsek@glanzkinder.com>
+ * @copyright 2013-2014 GLANZKINDER GmbH
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version   0.1
+ * @link      https://github.com/linslin
+ *
+ */
+
+
+
+/** ################## class params ################## **/
+
+	/**
+	 * Current page slider page as object
+	 * @var object
+	 */
+	var currentSliderPage = null;
+	
+	/**
+	 * holds count of elements to overjump due to 
+	 * page slider slideToPage left direction
+	 * @var integer
+	 */
+	var globalCount = 0;
+
+	
+
+/** ################## class methods ################## **/
+
+var pagingSlider = {	
+		
+	/**
+	 * Sliding a single page to another
+	 * 
+	 * @param string viewID
+	 * @param string pageId
+	 * @param string from
+	 */
+	slidePageFrom: function(viewId, pageId, from)
+	{
+		
+		//Init function var
+		var direction = '';
+		var fullPageId = '';
+		var nextPageId = null;
+		
+		switch(from){
+		
+			case 'left':
+				direction = 'right';
+				nextPageId = parseInt($(viewId + ' .slidepage-container .page.current').attr('page')) - 1;
+				fullPageId = pageId + nextPageId;
+			break;
+			
+			case 'right':
+				direction = 'left';
+				nextPageId = parseInt($(viewId + ' .slidepage-container .page.current').attr('page')) + 1;
+				fullPageId = pageId + nextPageId;
+			break;
+		}
+		
+		//get next page
+		page = $('#' + fullPageId);
+		
+		
+		if(page.length > 0){
+			
+			//switch css classes
+		    $(page).attr('class', "page current" + from); // Position the page at the starting position of the animation
+		    $(page).attr('class', "page current transition center");  // Position the new page and the current page at the ending position of their animation with a transition class indicating the duration of the animation
+
+		    //setup current page
+		    $(currentSliderPage).attr('class', "page transition " + direction);
+		    currentSliderPage = $(page);	
+		    
+		   //setup current page pointer //@Todo this could be lines with nice selector and without a loop.. later
+		    $(viewId + ' .footer-menu .pager li').each(function(i, e){
+		    	if(i+1 === nextPageId){
+		    		$(e).removeClass('inactive');
+		    		$(e).addClass('active');
+		    	}else{
+		    		$(e).removeClass('active');
+		    		$(e).removeClass('inactive');
+		    		$(e).addClass('inactive');
+		    	}
+		    });
+		}
+	},
+	
+	
+	/**
+	 * Switch to a specific page
+	 * 
+	 * @param object sliderPageContainer jquery object of 
+	 * @param string pageId pageId as string
+	 */
+	switchToPage: function(sliderPageContainer, pageId)
+	{
+		//Init vars
+		var pages = sliderPageContainer.find('.page');
+		var currentPage =  sliderPageContainer.find(".current");
+		var maxPageIndex = parseInt(pages.length-1);
+		var sickDif = (maxPageIndex-parseInt(currentPage.attr('page')));
+		var watchDiv = (maxPageIndex+1) -(parseInt(currentPage.attr('page')));
+		
+		if(parseInt(pageId) > parseInt(currentPage.attr('page'))){
+			
+			pages.each(function(i, e){
+				if( i+1 >= parseInt(currentPage.attr('page'))){
+					setTimeout(function() {
+						pagingSlider.switchIt(e, i, pageId, 'right', null, sliderPageContainer, currentPage);
+					}, (300*((i+1)-currentPage.attr('page'))));
+				}
+			});
+			
+		}else if (parseInt(pageId) < parseInt(currentPage.attr('page'))) {
+			
+			if(parseInt(currentPage.attr('page')) !== (maxPageIndex+1)){
+				rightDif = sickDif;
+			}else{
+				rightDif = sickDif;
+			}			
+			
+			//RESET clogbal count
+			globalCount = 0;
+			
+			pages.each(function(i, e){
+				
+				if(i < parseInt(currentPage.attr('page'))){
+					setTimeout(function() {
+						pagingSlider.switchIt(pages[(maxPageIndex-(i)-(watchDiv))], i, pageId, 'left', maxPageIndex, sliderPageContainer, currentPage);
+					}, (300*((i))));
+				}
+			});
+		}
+	},
+
+
+	/**
+	 * Switch an specific element
+	 * 
+	 * @param object e
+	 * @param interger i
+	 * @param string pageId
+	 * @param string direction
+	 * @param integer maxPageIndex
+	 * @param object sliderPageContainer
+	 * @param object currentPage
+	 */
+	switchIt: function(e, i, pageId, direction, maxPageIndex, sliderPageContainer, currentPage)
+	{
+		
+		var currentPageId = parseInt($(currentPage).attr('page'));
+		var sickVar = (currentPageId-parseInt(pageId));
+		
+		if(i+1 < pageId && direction === 'right'){
+			
+			$(e).next('.page').attr('class', "page current left");
+			$(e).next('.page').attr('class', "page current transition center");
+			$(e).attr('class', "page transition left");
+			currentSliderPage = $(e).next('.page').first();	 //set element as current global
+			
+		}else if(globalCount < sickVar  && i-1 < (maxPageIndex-parseInt(pageId)) && direction === 'left'){
+			
+			//count up if direction "left"
+			globalCount++;
+			
+			$(e).prev('.page').attr('class', "page current right");
+			$(e).prev('.page').attr('class', "page current transition center");
+			$(e).attr('class', "page transition right"); //set element as current global
+			currentSliderPage = $(e).prev('.page').first();	
+		}
+		
+		//setup current page pointer //@Todo this could be lines with nice selector and without a loop.. later
+		$('.footer-menu .pager li').each(function(i, e){
+			if(i+1 === parseInt(currentSliderPage.attr('page'))){
+				$(e).removeClass('inactive');
+				$(e).addClass('active');
+			}else{
+				$(e).removeClass('active');
+				$(e).removeClass('inactive');
+				$(e).addClass('inactive');
+			}
+		});
+	}
+};
