80785ae633ea351613b1e8bb39e94f2f997b3cbd
Nils Gajsek <nils.gajsek@glanzkinder.com>
2014-01-23 20:13:47 +0100

    Part of class optimizing

--- app/pagingSlider.js
+++ app/pagingSlider.js
@@ -8,6 +8,8 @@
  * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
  * @version   0.1
  * @link      https://github.com/linslin
+ * 
+ * @todo remove class page binder from init, currentPage can be set on any run
  *
  */
 
@@ -21,6 +23,12 @@ var pagingSlider = {
      * @var object
      */
     currentPage : null,
+    
+    /**
+     * private - Current page slider page as object
+     * @var object
+     */
+    _currentPage : null,
         
     /**
      * private - disabled status. Will be set to true while swiping -> dont touch
@@ -46,11 +54,14 @@ var pagingSlider = {
      * @param string sliderPageContainer
      * @param string from
      */
-    slidePageFrom: function(sliderPageContainer, from)
+    From: function(sliderPageContainer, from)
     {
         
         //Init function var
         var direction = '';
+        
+        //set current page
+        this._updateCurrentPage(sliderPageContainer);
         
         //check if disabled -> swipe is disabled while animation swipe
         if(pagingSlider._disabled === true){
@@ -61,12 +72,12 @@ var pagingSlider = {
         switch(from){
             case 'left':
                 direction = 'right';
-                page = $(sliderPageContainer + ' .slidepage-container .page.current').prev().first();
+                page = $(sliderPageContainer + ' .page.current').prev().first();
             break;
             
             case 'right':
                 direction = 'left';
-                page = $(sliderPageContainer + ' .slidepage-container .page.current').next().first();
+                page = $(sliderPageContainer + ' .page.current').next().first();
             break;
         }
        
@@ -76,22 +87,13 @@ var pagingSlider = {
             //switch css classes
             $(page).attr('class', "page current" + from); // Position the page at the starting position of the animation
             $(page).attr('class', "page current transition center");  // Position the new page and the current page at the ending position of their animation with a transition class indicating the duration of the animation
-            $(pagingSlider.currentPage).attr('class', "page transition " + direction);
+            $(pagingSlider._currentPage).attr('class', "page transition " + direction);
             
             //setup current page
-            pagingSlider.currentPage = page;    
+            pagingSlider._currentPage = page;   
             
-           //setup current page pointer //@Todo this could be lines with nice selector and without a loop.. later
-            $(sliderPageContainer + ' .footer-menu .pager li').each(function(i, e){
-                if(i+1 === parseInt(page.attr('page'))){
-                    $(e).removeClass('inactive');
-                    $(e).addClass('active');
-                }else{
-                    $(e).removeClass('active');
-                    $(e).removeClass('inactive');
-                    $(e).addClass('inactive');
-                }
-            });
+            //setup page browser
+            pagingSlider._updatePagePointer($(sliderPageContainer), parseInt(page.attr('page')));
         }
     },
     
@@ -99,27 +101,26 @@ var pagingSlider = {
     /**
      * Switch to a specific page
      * 
-     * @param object sliderPageContainer jquery object of 
+     * @param object sliderPageContainer jquery object
      * @param integer pageId pageId as string
      */
     switchToPage: function(sliderPageContainer, pageId)
     {
         
         //Init vars
-        var pages = sliderPageContainer.find('.page');
-        var currentPage =  sliderPageContainer.find(".current");
+        var pages = $(sliderPageContainer).find('.page');
+        var currentPage =  $(sliderPageContainer).find(".current");
         var maxPageIndex = parseInt(pages.length-1);
         var sickDif = (maxPageIndex-parseInt(currentPage.attr('page')));
         var watchDiv = (maxPageIndex+1) -(parseInt(currentPage.attr('page')));
         
         //check if _disabled -> swipe is _disabled while animation swipe, also return if the current page is clicked
-        if(pagingSlider._disabled === true && parseInt(currentPage.attr('page')) === pageId){
+        if(pagingSlider._disabled === true || parseInt(currentPage.attr('page')) === pageId){
             return;
         }
         
         //disable swiping
         pagingSlider._disabled = true;
-        
         
         if(pageId > parseInt(currentPage.attr('page'))){
             
@@ -171,13 +172,13 @@ var pagingSlider = {
     /**
      * Switch an specific element
      * 
-     * @param object e
+     * @param object   e
      * @param interger i
-     * @param integer pageId
-     * @param string direction
-     * @param integer maxPageIndex
-     * @param object sliderPageContainer
-     * @param object currentPage
+     * @param integer  pageId
+     * @param string   direction
+     * @param integer  maxPageIndex
+     * @param object   sliderPageContainer
+     * @param object   currentPage
      */
     switchIt: function(e, i, pageId, direction, maxPageIndex, sliderPageContainer, currentPage)
     {
@@ -203,7 +204,7 @@ var pagingSlider = {
             pagingSlider.currentPage = $(e).prev('.page').first();    
         }
         
-        //setup current page pointer //@Todo this could be lines with nice selector and without a loop.. later
+        //setup current page pointer //@Todo slidePage could be lines with nice selector and without a loop.. later
         $('.footer-menu .pager li').each(function(i, e){
             if(i+1 === parseInt($(pagingSlider.currentPage).attr('page'))){
                 $(e).removeClass('inactive');
@@ -226,12 +227,15 @@ var pagingSlider = {
     {
     	
     	//Init
-        var pages = sliderPageContainer.find('.page');
+        var pages = $(sliderPageContainer).find('.page');
 
         //check if disabled -> swipe is disabled while animation swipe
         if(pagingSlider._disabled === true){
             return;
         }
+        
+        //set current page
+        pagingSlider._updateCurrentPage(sliderPageContainer);
         
         //set page classes
         pages.each(function(i, e){
@@ -248,7 +252,29 @@ var pagingSlider = {
         pagingSlider.currentPage = pages.get(pageId-1);  
         
         //setup page browser
+        pagingSlider._updatePagePointer($(sliderPageContainer), pageId);
+    },
+    
+    
+    /**
+     * Auto update function for footer
+     * 
+     * @param object sliderPageContainer jquery object 
+     * @param integer pageId pageId as string
+     */
+    _updatePagePointer: function(sliderPageContainer, pageId)
+    {
         sliderPageContainer.next('.footer-menu').find('.pager li.active').removeClass('active').addClass('inactive');
         $(sliderPageContainer.next('.footer-menu').find('.pager li').get(pageId-1)).removeClass('inactive').addClass('active');
+    },
+    
+    
+    /**
+     * Set current active page
+     * @param object sliderPageContainer jquery object  
+     */
+    _updateCurrentPage: function(sliderPageContainer)
+    {
+    	pagingSlider._currentPage =  $(sliderPageContainer + ' .page.current');
     }
 };
