9440361fd7023fa7f5dc7d0c9ef56a1e9be683f3
Nils Gajsek <nils.gajsek@glanzkinder.com>
2014-02-02 14:13:54 +0100

    Added todo

--- app/pagingSlider.js
+++ app/pagingSlider.js
@@ -12,6 +12,7 @@
  * @todo page ids removen um löschunen von divs zu erlauben
  * @todo add loop wenn letztes element erreicht/infity scroller
  * @todo footer div konfigurierbar machen für position und ein-/ausschalten
+ * @todo remove inactive class, no class will be "default" for pageItemPointers
  *
  */
 
